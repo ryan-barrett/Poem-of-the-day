1498a7724bfa57898d97fff26115de784c01cc9c
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-01-17 18:01:15 -0800

    added twilio and continued server.js setup

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
 var express = require("express"),
   bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static("public"));
+
+var twilio = require("twilio");
+
+// Find your account sid and auth token in your Twilio account Console.
+var client = new twilio("TWILIO_ACCOUNT_SID", "TWILIO_AUTH_TOKEN");
+
+// Send the text message.
+client.messages.create({
+  to: "YOUR_NUMBER",
+  from: "YOUR_TWILIO_NUMBER",
+  body: "Hello from Twilio!"
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("poem of the day listening at http://localhost:3000/");
+});
